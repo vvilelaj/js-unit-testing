79dd691b65ff4caef037f5008094a660ea72a634
Victor Vilela <victor.vilela@outlook.com>
2021-03-06 00:26:57 -0500

    updates variable names from request.body

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require("body-parser")
 var methodOverride = require("method-override");
+var cors = require('cors')
 
 var app = express()
 var port = 3001
@@ -9,6 +10,7 @@ var router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(cors());
 
 app.use(router);
 router.get('/', function (req, res) {
@@ -20,11 +22,11 @@ auth.route('/auth/login')
   .post((req, res) => {
     console.log('POST');
     console.log(req.body);
-    if (req.body.user == "vvilelaj" &&
-      req.body.pass == "vvilelaj")
-      res.status(200).jsonp({ success: true });
+    if (req.body.username == "vvilelaj" &&
+      req.body.password == "vvilelaj")
+      res.status(200).jsonp({ success: true, data: {} });
     else
-      res.status(403).jsonp({ success: false });
+      res.status(403).jsonp({ success: false, message: 'invalid credentials' });
   });
 app.use('/api', auth);
 
